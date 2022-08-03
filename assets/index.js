4ea59a9c400a65a9685fd5cfc6c93098666bde3c
IXOR <igormessiaen98@gmail.com>
2022-08-04 01:12:22 +0300

    in

---
+++ assets/index.js
@@ -0,0 +1,14 @@
+$(".banner__slider").slick({
+  arrows: false,
+  dots: true,
+  dotsClass: "banner__dots",
+});
+
+[...document.querySelector(".banner__dots").children].forEach(
+  (el) => (el.textContent = "")
+);
+
+document.querySelector(".header__barsbox").addEventListener("click", (e) => {
+  document.querySelector("aside.aside").classList.toggle("active");
+  document.body.classList.toggle("overflow-hidden-mobile");
+});
